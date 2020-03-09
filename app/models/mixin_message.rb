826658f45660fb084612b5d7b857f5c18793d301
an-lee <an.lee.work@gmail.com>
2020-03-09 17:22:38 +0800

    :sparkles: (mixin_message) handle mixin message

--- app/models/mixin_message.rb
+++ app/models/mixin_message.rb
@@ -24,4 +24,29 @@
 #  index_mixin_messages_on_user_id     (user_id)
 #
 class MixinMessage < ApplicationRecord
+  before_validation :set_attributes, on: :create
+
+  validates :message_id, presence: true
+  validates :raw, presence: true
+
+  def plain?
+    /^PLAIN_/.match? category
+  end
+
+  private
+
+  def set_attributes
+    return unless new_record?
+
+    data = raw['data']
+
+    self.action            = raw['action']
+    self.message_id        = data['message_id']
+    self.category          = data['category']
+    self.conversation_id   = data['conversation_id']
+    self.quote_message_id  = data['quote_message_id']
+    self.representative_id = data['representative_id']
+    self.user_id           = data['user_id']
+    self.content           = Base64.decode64 data['data']
+  end
 end
