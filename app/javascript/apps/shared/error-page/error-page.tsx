9e3d61c1d357f4546adf7f457b75d7a459df92a1
an-lee <an.lee.work@gmail.com>
2020-03-09 17:24:51 +0800

    :fire: (shared) remove loging/error page

--- app/javascript/apps/shared/error-page/error-page.tsx
+++
@@ -1,6 +0,0 @@
-import { Result } from 'antd';
-import React from 'react';
-
-export function ErrorPage() {
-  return <Result status='error' title='Opps...' />;
-}
--- app/javascript/apps/shared/index.ts
+++ app/javascript/apps/shared/index.ts
@@ -1,3 +1,2 @@
 export * from './interfaces';
 export * from './client';
-export * from './loading-page';
