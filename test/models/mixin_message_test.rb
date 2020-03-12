4dc21895c71af74f135ab14a3480455dc51c65d7
an-lee <an.lee.work@gmail.com>
2020-03-12 17:00:17 +0800

    :white_check_mark: (mixin_message) add model test

--- test/models/mixin_message_test.rb
+++ test/models/mixin_message_test.rb
@@ -26,7 +26,37 @@
 require 'test_helper'
 
 class MixinMessageTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @raw = {
+      id: 'dc9b20d1-807c-4778-9b66-02a04db08a13',
+      action: 'CREATE_MESSAGE',
+      data:
+        {
+          type: 'message',
+          primitive_id: '',
+          primitive_message_id: '',
+          representative_id: '',
+          quote_message_id: '',
+          conversation_id: '204c0633-ef55-38c3-bbf7-4069cd6661bb',
+          user_id: '7ed9292d-7c95-4333-aa48-a8c640064186',
+          session_id: '',
+          message_id: '71474673-b0d0-4f8b-ac33-fdf777c3f686',
+          category: 'PLAIN_TEXT',
+          data: 'aHR0cDovLzE5Mi4xNjguMC43NjozMDAw',
+          status: 'SENT',
+          source: 'LIST_PENDING_MESSAGES',
+          created_at: '2019-10-09T01:52:51.386567Z',
+          updated_at: '2019-10-09T01:52:51.386567Z'
+        }
+    }
+  end
+
+  test 'create mixin message' do
+    msg = MixinMessage.create! raw: @raw
+    assert msg.message_id == @raw[:data][:message_id]
+  end
+
+  test 'plain category' do
+    assert mixin_messages(:plain_text).plain?
+  end
 end
