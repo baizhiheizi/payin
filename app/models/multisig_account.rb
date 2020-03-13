b0a78563d7a8f4d2c39541e0d8b1bed9674388ea
an-lee <an.lee.work@gmail.com>
2020-03-13 14:59:11 +0800

    :sparkles: (model design) add multisig utxo

--- app/models/multisig_account.rb
+++ app/models/multisig_account.rb
@@ -26,11 +26,29 @@ class MultisigAccount < ApplicationRecord
   has_many :multisig_account_members, dependent: :nullify
   has_many :members, through: :multisig_account_members, source: :user
 
+  before_validation :set_hash
+
   validates :name, presence: true
   validates :threshold, numericality: { only_integer: true, greater_than: 0 }
-  validates :members, presence: true
+  validates :member_uuids, presence: true
+  validates :hash, presence: true, uniqueness: true
   validate :threshold_must_not_greater_than_members_size
   validate :members_must_be_valid_array
+
+  def utxos
+    records = MixinBot.api.get_all_multisigs access_token: creator.access_token
+    records
+      .select(&->(utxo) { utxo['members'].sort == member_uuids.sort })
+      .map(&->(record) { MultisigUtxo.new(record) })
+  end
+
+  def assets
+    Asset.where(asset_id: utxos.map(&:asset_id).uniq)
+  end
+
+  def balance(asset_id)
+    utxos.select(&->(utxo) { utxo.asset_id == asset_id }).map(&:amount).sum
+  end
 
   private
 
@@ -41,5 +59,10 @@ class MultisigAccount < ApplicationRecord
   def members_must_be_valid_array
     erros.add(:members, 'Members must be an array') unless members.is_a? Array
     erros.add(:members, 'Members size must be greater than 0') if members.length <= 0
+  end
+
+  def set_hash
+    self.member_uuids = member_uuids.sort
+    self.hash = Digest::MD5.hexdigest threshold.to_s + member_uuids.join
   end
 end
