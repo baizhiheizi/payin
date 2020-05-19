b532c6b535eaed67cc7b83c0297333e53ceeb946
an-lee <an.lee.work@gmail.com>
2020-05-19 22:23:01 +0800

    :heavy_plus_sign: (babel) use babel instead ts-loader

--- app/javascript/apps/shared/mixin-utils.ts
+++ app/javascript/apps/shared/mixin-utils.ts
@@ -1,61 +1,62 @@
-function MixinUtils() {}
-
-MixinUtils.prototype = {
-  environment: function () {
+class MixinUtils {
+  public environment() {
     if (
-      (<any>window).webkit &&
-      (<any>window).webkit.messageHandlers &&
-      (<any>window).webkit.messageHandlers.MixinContext
+      (window as any).webkit &&
+      (window as any).webkit.messageHandlers &&
+      (window as any).webkit.messageHandlers.MixinContext
     ) {
       return 'iOS';
     }
-    if ((<any>window).MixinContext && (<any>window).MixinContext.getContext) {
+    if (
+      (window as any).MixinContext &&
+      (window as any).MixinContext.getContext
+    ) {
       return 'Android';
     }
     return undefined;
-  },
+  }
 
-  conversationId: function () {
+  public conversationId() {
     let ctx: any;
     switch (this.environment()) {
       case 'iOS':
         ctx = prompt('MixinContext.getContext()');
         return JSON.parse(ctx).conversation_id;
       case 'Android':
-        ctx = (<any>window).MixinContext.getContext();
+        ctx = (window as any).MixinContext.getContext();
         return JSON.parse(ctx).conversation_id;
       default:
         return undefined;
     }
-  },
+  }
 
-  appVersion: function () {
+  public appVersion() {
     let ctx: any;
     switch (this.environment()) {
       case 'iOS':
         ctx = prompt('MixinContext.getContext()');
         return JSON.parse(ctx).app_version;
       case 'Android':
-        ctx = (<any>window).MixinContext.getContext();
+        ctx = (window as any).MixinContext.getContext();
         return JSON.parse(ctx).app_version;
       default:
         return undefined;
     }
-  },
+  }
 
-  immersive: function () {
+  public immersive() {
     let ctx: any;
     switch (this.environment()) {
       case 'iOS':
         ctx = prompt('MixinContext.getContext()');
         return JSON.parse(ctx).immersive;
       case 'Android':
-        ctx = (<any>window).MixinContext.getContext();
+        ctx = (window as any).MixinContext.getContext();
         return JSON.parse(ctx).immersive;
       default:
         return undefined;
     }
-  },
-};
+  }
+}
 
 export const mixinUtils = new MixinUtils();
--- config/webpack/loaders/less-loader.js
+++ config/webpack/loaders/less-loader.js
@@ -5,14 +5,19 @@ module.exports = {
   test: /\.(less)$/i,
   use: [
     {
-      loader: "style-loader"
+      loader: 'style-loader',
     },
     {
-      loader: "css-loader" // translates CSS into CommonJS
+      loader: 'css-loader', // translates CSS into CommonJS
     },
     {
-      loader: "less-loader", // compiles Less to CSS
-      options: { modifyVars: theme, javascriptEnabled: true }
-    }
-  ]
+      loader: 'less-loader', // compiles Less to CSS
+      options: {
+        lessOptions: {
+          modifyVars: theme,
+          javascriptEnabled: true,
+        },
+      },
+    },
+  ],
 };
